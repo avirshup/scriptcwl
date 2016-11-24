d78724138f94eca0eca2538d39a65b89941a774e
arnikz <a.kuzniar@esciencecenter.nl>
2016-11-24 16:04:49 +0100

    Minor fixes

--- scriptcwl/workflow.py
+++ scriptcwl/workflow.py
@@ -12,33 +12,33 @@ from .yamlmultiline import str_presenter, is_multiline
 class WorkflowGenerator(object):
 
     def __init__(self, steps_dir=None):
-        self.wf_steps = {}
-        self.wf_inputs = {}
-        self.wf_outputs = {}
-        self.step_output_types = {}
-        self.steps_library = {}
+        self._wf_steps = {}
+        self._wf_inputs = {}
+        self._wf_outputs = {}
+        self._step_output_types = {}
+        self._steps_library = {}
 
         if steps_dir is not None:
             steps = load_steps(steps_dir)
             self.load(steps)
 
     def __getattr__(self, name, **kwargs):
-        name = cwl_name(name)
+        name = self._cwl_name(name)
         step = self._get_step(name)
         for n in step.output_names:
             oname = step.output_to_input(n)
-            self.step_output_types[oname] = step.step_outputs[n]
+            self._step_output_types[oname] = step.step_outputs[n]
         return partial(self._make_step, step, **kwargs)
 
     def load(self, steps):
         for n, step in steps.iteritems():
-            if n in self.steps_library.keys():
+            if n in self._steps_library.keys():
                 print 'WARNING: step "{}" already in steps library'.format(n)
             else:
-                self.steps_library[n] = step
+                self._steps_library[n] = step
 
     def list_steps(self):
-        for name, step in self.steps_library.iteritems():
+        for name, step in self._steps_library.iteritems():
             print 'Step "{}": {}'.format(name, step)
 
     def inputs(self, name):
@@ -48,7 +48,7 @@ class WorkflowGenerator(object):
         print s.list_inputs()
 
     def _add_step(self, step):
-        self.wf_steps[step.name] = step.to_obj()
+        self._wf_steps[step.name] = step.to_obj()
 
     def add_inputs(self, **kwargs):
         """Add workflow inputs.
@@ -61,7 +61,7 @@ class WorkflowGenerator(object):
         """
         names = []
         for name, typ in kwargs.iteritems():
-            self.wf_inputs[name] = typ
+            self._wf_inputs[name] = typ
             names.append(name)
 
         if len(names) == 1:
@@ -80,14 +80,14 @@ class WorkflowGenerator(object):
         for name, source_name in kwargs.iteritems():
             obj = {}
             obj['outputSource'] = source_name
-            obj['type'] = self.step_output_types[source_name]
-            self.wf_outputs[name] = obj
+            obj['type'] = self._step_output_types[source_name]
+            self._wf_outputs[name] = obj
 
     def set_documentation(self, doc):
         self.documentation = doc
 
     def _get_step(self, name):
-        s = self.steps_library.get(name)
+        s = self._steps_library.get(name)
         if s is None:
             msg = '"{}" not found in steps library. Please check your ' \
                   'spelling or load additional steps'
@@ -102,9 +102,9 @@ class WorkflowGenerator(object):
             obj['doc'] = self.documentation
         except (AttributeError, ValueError):
             pass
-        obj['inputs'] = self.wf_inputs
-        obj['outputs'] = self.wf_outputs
-        obj['steps'] = self.wf_steps
+        obj['inputs'] = self._wf_inputs
+        obj['outputs'] = self._wf_outputs
+        obj['steps'] = self._wf_steps
         return obj
 
     def to_script(self, wf_name='wf'):
@@ -122,7 +122,7 @@ class WorkflowGenerator(object):
         # Workflow inputs
         params = []
         returns = []
-        for name, typ in self.wf_inputs.iteritems():
+        for name, typ in self._wf_inputs.iteritems():
             params.append('{}=\'{}\''.format(name, typ))
             returns.append(name)
         print '{} = {}.add_inputs({})'.format(', '.join(returns), wf_name,
@@ -130,7 +130,7 @@ class WorkflowGenerator(object):
 
         # Workflow steps
         returns = []
-        for name, step in self.wf_steps.iteritems():
+        for name, step in self._wf_steps.iteritems():
             s = Step(step['run'])
             returns = ['{}_{}'.format(python_name(s.name), o) for o in step['out']]
             params = ['{}={}'.format(name, python_name(param)) for name, param in step['in'].iteritems()]
@@ -138,7 +138,7 @@ class WorkflowGenerator(object):
 
         # Workflow outputs
         params = []
-        for name, details in self.wf_outputs.iteritems():
+        for name, details in self._wf_outputs.iteritems():
             params.append('{}={}'.format(name, python_name(details['outputSource'])))
         print '{}.add_outputs({})'.format(wf_name, ', '.join(params))
 
@@ -164,10 +164,10 @@ class WorkflowGenerator(object):
         with codecs.open(fname, 'wb', encoding=encoding) as yaml_file:
             yaml.dump(self.to_obj(), yaml_file, default_flow_style=False)
 
+    @staticmethod
+    def _cwl_name(name):
+        """Transform python name to cwl name.
+        """
+        name = name.replace('_', '-')
 
-def cwl_name(name):
-    """Transform python name to cwl name.
-    """
-    name = name.replace('_', '-')
-
-    return name
+        return name
